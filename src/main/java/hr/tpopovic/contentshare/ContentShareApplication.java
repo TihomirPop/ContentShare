6000e08f3e5bb274e1a0fbc392d03d83e650457e
TihomirPop <95313780+TihomirPop@users.noreply.github.com>
2025-08-15 13:17:05 +0200

    init

---
+++ src/main/java/hr/tpopovic/contentshare/ContentShareApplication.java
@@ -0,0 +1,13 @@
+package hr.tpopovic.contentshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tpopovic/contentshare/ContentShareApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tpopovic.contentshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
